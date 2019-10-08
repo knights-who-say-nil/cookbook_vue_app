65c7dcb69af219a6319ce38fca277c201ad01bd2
JackMarx <seriousfools@gmail.com>
2019-10-08 11:37:20 -0500

    new part 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
+    <div>
+      <h1>New Recipe</h1>
+      <ul>
+        <li v-for="error in errors">{{ error }}</li>
+      </ul>
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
+
     <h1>All Recipes</h1>
 
     <div v-for="recipe in recipes">
       <h2>{{ recipe.title }}</h2>
       <p>Ingredients: {{ recipe.ingredients }}</p>
       <p>Directions: {{ recipe.directions }}</p>
+      
+      <img v-bind:src="recipe.image_url">
       <br>
     </div>
 
@@ -13,6 +23,9 @@
 </template>
 
 <style>
+img {
+  width: 250px;
+}
 </style>
 
 <script>
@@ -21,7 +34,8 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      recipes: []
+      recipes: [],
+      errors: []
     };
   },
   created: function() {
@@ -31,6 +45,30 @@ export default {
         this.recipes = response.data;
       });
   },
-  methods: {}
+  methods: {
+    createRecipe: function() {
+      console.log("Create the recipe...");
+
+      var clientParams = {
+        title: "example Title 3",
+        chef: "example Chef",
+        prep_time: "example Prep Time",
+        ingredients: "example Ingredients", 
+        directions: "example Directions",
+        image_url: "example Image URL"
+      };
+
+      axios
+        .post("/api/recipes", clientParams)
+        .then(response => {
+          console.log("success", response.data);
+          this.recipes.push(response.data);
+          this.errors = [];
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
 };
 </script>
